c17c7b07be55b4e7cd8210fae43e32d163dbfa6b
toshio <toshio@oYo>
2022-04-28 12:49:43 +0900

    calamaresでインストール時に実行するコマンドを追加

---
+++ root/usr/share/openyellowos/calamares.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+# remove Polkit config for live user
+rm -f /etc/polkit-1/localauthority/50-local.d/47-allow-live-user.pkla
+
+# remove nopassword
+rm -f /etc/sudoers.d/wheel
+
